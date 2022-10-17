3a611c821821fe47eddec354aac07e5d8294c119
thisismikem <maimao@gmail.com>
2022-10-17 13:24:03 -0400

    Thirddd commit

--- src/test/java/com/example/docker/SpringBootDemoApplicationTest.java
+++ src/test/java/com/example/docker/SpringBootDemoApplicationTest.java
@@ -3,9 +3,14 @@ package com.example.docker;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class SpringBootDemoApplicationTest {
 
 	static Logger logger = LoggerFactory.getLogger(SpringBootDemoApplicationTest.class);
