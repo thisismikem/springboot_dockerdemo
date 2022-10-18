09aca3e532ba35bc60caf2d48fa18ae78887f05f
thisismikem <maimao@gmail.com>
2022-10-18 14:01:40 -0400

    Added error

--- src/main/java/com/example/docker/HelloWorldController.java
+++ src/main/java/com/example/docker/HelloWorldController.java
@@ -1,16 +1,28 @@
 package com.example.docker;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/rest")
 public class HelloWorldController {
 	
-	@GetMapping("/get")
+	@GetMapping("/getDocker")
 	public String sayHello() {
 		return "Hello Spring Boot Docker";
+	}
+	
+	@GetMapping("/getName")
+	public String getName() {
+		return "Hello Michael";
+	}
+	
+	@GetMapping("/getSomething")
+	public String getSomething() {
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 	}
 	
 }
